8f8b2e8689cb327089b66d59f6f22b39ad2073be
gongyouqiang <gongyouqiang@stary.ltd>
2024-05-27 15:29:08 +0800

    swift 弹窗气泡支持package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "BTBubble",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BTBubble",
             targets: ["BTBubble"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "BTBubble"),
-        .testTarget(
-            name: "BTBubbleTests",
-            dependencies: ["BTBubble"]),
     ]
 )
